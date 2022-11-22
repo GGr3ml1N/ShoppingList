7444d81b5d4a8852bc5c5152c2a742a56efdff48
GGr3ml1N <dinars163@gmail.com>
2022-11-23 02:05:34 +0400

    Created new abstract class "MainDatabase". Added companion object fun getDatabase() : MainDatabase

---
+++ app/src/main/java/com/ggr3ml1n/shoppinglist/db/MainDatabase.kt
@@ -0,0 +1,29 @@
+package com.ggr3ml1n.shoppinglist.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.ggr3ml1n.shoppinglist.entities.LibraryItem
+import com.ggr3ml1n.shoppinglist.entities.NoteItem
+import com.ggr3ml1n.shoppinglist.entities.ShoppingListItem
+import com.ggr3ml1n.shoppinglist.entities.ShoppingListNames
+
+@Database (entities = [LibraryItem::class, NoteItem::class, ShoppingListItem::class, ShoppingListNames::class], version = 1)
+abstract class MainDatabase : RoomDatabase() {
+    
+    companion object {
+        @Volatile
+        private var INSTANCE: MainDatabase? = null
+        fun getDatabase(context: Context) : MainDatabase {
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MainDatabase::class.java,
+                    "shopping_list.db",
+                ).build()
+                instance
+            }
+        }
+    }
+}
